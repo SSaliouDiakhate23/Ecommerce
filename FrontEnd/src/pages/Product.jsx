e5a740846e1539f82e3b61291dca4df99495a651
Serigne Saliou Diakhate <mac@MBP-de-Mac-2.lan>
2024-11-09 13:38:29 +0000

    Product pages

--- FrontEnd/src/pages/Product.jsx
+++ FrontEnd/src/pages/Product.jsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/frontend_assets/assets";
 
 function Product() {
   const { productId } = useParams();
-  const { products } = useContext(ShopContext);
+  const { products, currency } = useContext(ShopContext);
   const [product, setProduct] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [image, setImage] = useState(null);
+  const [selectedSize, setSelectedSize] = useState("");
 
   async function fetchProduct() {
     setIsLoading(true);
@@ -14,6 +17,7 @@ function Product() {
       const foundProduct = products.find((item) => item._id === productId);
       if (foundProduct) {
         setProduct(foundProduct);
+        setImage(foundProduct.image[0]);
       } else {
         console.log("Product not found");
       }
@@ -26,14 +30,93 @@ function Product() {
 
   useEffect(() => {
     fetchProduct();
-  }, [productId]);
+  }, [productId, product]);
 
   return isLoading ? (
     <div className="flex justify-center items-center h-screen">
       <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
     </div>
   ) : (
-    <div>Product {productId}</div>
+    <>
+      <hr className="w-full bg-gray-900 mb-6 md:mb-10" />
+      <div className="container mx-auto px-4 flex flex-col-reverse md:flex-row items-start gap-4 md:gap-8 lg:gap-10 transition-all duration-300 ease-in-out">
+        {/* Thumbnails Gallery */}
+        <div className="w-full md:w-24 lg:w-32 transition-all duration-300">
+          <div className="flex  flex-row md:flex-col gap-2 md:gap-4 items-center justify-center">
+            {product.image.map((item, index) => (
+              <img
+                key={index}
+                src={item}
+                alt={product.name}
+                className="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 object-cover cursor-pointer transform transition-all duration-300 ease-in-out hover:opacity-80 hover:scale-105"
+                onClick={() => setImage(item)}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Main Image */}
+        <div className="w-full md:w-1/2 lg:w-2/5 cur h-auto mb-6 md:mb-0 transition-all duration-500 ease-in-out transform hover:scale-[1.02]">
+          <img
+            src={image}
+            alt={product.name}
+            className="w-full h-auto object-cover transition-opacity duration-300"
+          />
+        </div>
+
+        {/* Product Info */}
+        <div className="w-full md:w-1/3 lg:w-2/4 px-4 transition-transform duration-300 ease-in-out">
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold transform transition-all duration-300 hover:translate-x-1">
+            {product.name}
+          </h1>
+
+          <div className="flex items-center gap-1 mt-2 md:mt-3">
+            {[...Array(5)].map((_, index) => (
+              <img
+                key={index}
+                src={assets.star_icon}
+                alt="star"
+                className="w-3 h-3 md:w-4 md:h-4"
+              />
+            ))}
+          </div>
+
+          <p className="text-xl md:text-2xl lg:text-3xl font-bold mt-4 md:mt-6">
+            {currency}
+            {product.price}
+          </p>
+
+          <p className="text-sm md:text-base max-w-3xl lg:text-lg font-light mt-4 md:mt-6">
+            {product.description}
+          </p>
+
+          <p className="text-base md:text-lg font-medium my-4 md:my-6">
+            Select Size
+          </p>
+
+          <div className="flex flex-wrap gap-2">
+            {product.sizes.map((size, index) => (
+              <div
+                key={index}
+                className={`px-4 py-2 ${
+                  size === selectedSize
+                    ? "bg-black text-white"
+                    : "bg-gray-100 hover:bg-gray-200"
+                } cursor-pointer text-center rounded`}
+                onClick={() => setSelectedSize(size)}
+              >
+                <p className="text-sm md:text-base">{size}</p>
+              </div>
+            ))}
+          </div>
+          <button className="bg-gray-900 transition-all duration-300 ease-in-out hover:bg-white hover:text-black hover:border hover:border-black text-white px-8 py-3 rounded mt-4  md:mt-8">
+            Add to Cart
+          </button>
+
+          <hr className="w-full bg-black mb-6 my-6 md:my-10" />
+        </div>
+      </div>
+    </>
   );
 }
 
